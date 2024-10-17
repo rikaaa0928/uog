7d07c22665c03cb57fed72ba08d136a65ac3d5a7
rikaaa0928 <wangzhidong1@xiaomi.com>
2024-10-17 11:01:42 +0800

    rust init rustls_platform_verifier

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,19 @@ mod util;
 
 use std::future::Future;
 use tokio::runtime::Runtime;
+use jni::JNIEnv;
+use jni::objects::{JClass, JObject};
 
-uniffi::include_scaffolding!("uog");
+uniffi::setup_scaffolding!();
 
+#[uniffi::export]
 #[no_mangle]
 pub fn start_client(l_addr: &str, d_addr: &str, auth: &str) -> String {
     let rt = Runtime::new().unwrap();
     let r = rt.block_on(client::start(l_addr.to_owned(), d_addr.to_owned(), auth.to_owned()));
     if r.is_err() {
         let x = &r.err().unwrap();
-        return l_addr.clone().to_owned() + " : " + d_addr + " : " + auth + " : " + x.clone().to_string().as_str() + " : " + x.clone().backtrace().to_string().as_str();
+        return l_addr.clone().to_owned() + " : " + d_addr + " : " + auth + " : " + x.clone().to_string().as_str() + " : " + x.backtrace().to_string().as_str();
     } else {
         return "".to_string();
     }
@@ -24,6 +27,16 @@ pub fn start_client(l_addr: &str, d_addr: &str, auth: &str) -> String {
 //     let rt = Runtime::new().unwrap();
 //     let _ = rt.block_on(server::UogServer::bind(l_addr, d_addr, auth));
 // }
+
+
+#[allow(non_snake_case)]
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub extern "system" fn Java_moe_rikaaa0928_uot_Init_init(env: JNIEnv, _class: JClass, context: JObject) {
+    // Then, initialize the certificate verifier for future use.
+    let _ = rustls_platform_verifier::android::init_hosted(&env, context);
+}
+
 
 #[cfg(test)]
 mod tests {
