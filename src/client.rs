4a69f2307dcff70b01d05f1026fca1e1977c2884
rikaaa0928 <wangzhidong1@xiaomi.com>
2025-01-13 14:00:51 +0800

    log

--- src/client.rs
+++ src/client.rs
@@ -1,31 +1,30 @@
+use crate::util;
+use anyhow::Error;
+use hyper_util::client::legacy::connect::HttpConnector;
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::TokioExecutor;
+use log::{debug, error, info};
+use pb::udp_service_client::UdpServiceClient;
+use pb::UdpReq;
+use pb::UdpRes;
+use rustls::crypto::aws_lc_rs::default_provider;
+use rustls::crypto::{CryptoProvider, WebPkiSupportedAlgorithms};
 use std::env;
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::str::FromStr;
-use std::sync::{atomic, Arc};
 use std::sync::atomic::AtomicBool;
+use std::sync::{atomic, Arc};
 use std::time::Duration;
-use anyhow::Error;
-use hyper_util::client::legacy::Client;
-use hyper_util::client::legacy::connect::HttpConnector;
-use hyper_util::rt::TokioExecutor;
-use log::{debug, error};
-use rustls::crypto::{CryptoProvider, WebPkiSupportedAlgorithms};
-use rustls::crypto::aws_lc_rs::default_provider;
 use tokio::net::UdpSocket;
-use tokio::{io, spawn};
-use tokio::sync::{mpsc, oneshot, Mutex};
 use tokio::sync::oneshot::{Receiver, Sender};
+use tokio::sync::{mpsc, oneshot, Mutex};
 use tokio::time::timeout;
+use tokio::{io, spawn};
 use tokio_stream::StreamExt;
 use tonic::client::GrpcService;
-use tonic::Request;
 use tonic::transport::Uri;
-use pb::udp_service_client::UdpServiceClient;
-use pb::UdpReq;
-use pb::UdpRes;
-use crate::util;
-
+use tonic::Request;
 
 pub mod pb {
     tonic::include_proto!("dad.xiaomi.uog");
@@ -48,8 +47,12 @@ async fn interruptible_recv(
     }
 }
 
-
-pub async fn start(l_addr: String, d_addr: String, auth: String, global_int: &mut Receiver<()>) -> util::Result<()> {
+pub async fn start(
+    l_addr: String,
+    d_addr: String,
+    auth: String,
+    global_int: &mut Receiver<()>,
+) -> util::Result<()> {
     let sock = UdpSocket::bind(l_addr).await?;
     let sock = Arc::new(sock);
 
@@ -82,13 +85,11 @@ pub async fn start(l_addr: String, d_addr: String, auth: String, global_int: &mu
             })
             .service(http);
 
-        let client = hyper_util::client::legacy::Client::builder(TokioExecutor::new()).build(connector);
+        let client =
+            hyper_util::client::legacy::Client::builder(TokioExecutor::new()).build(connector);
         let mut client = UdpServiceClient::with_origin(client, uri);
         Arc::new(Mutex::new(client.start_stream(rx).await?.into_inner()))
     };
-
-    // let mut client = UdpServiceClient::with_origin(client, uri);
-    // let out_stream = Arc::new(Mutex::new(client.start_stream(rx).await?.into_inner()));
 
     let mut last_addr: Option<SocketAddr> = None;
     let should_stop = Arc::new(AtomicBool::new(false));
@@ -97,26 +98,48 @@ pub async fn start(l_addr: String, d_addr: String, auth: String, global_int: &mu
     // let interrupt_sender = Arc::new(Mutex::new(interrupt_sender));
     let mut buf = [0; 65536];
     while !should_stop.clone().load(atomic::Ordering::Relaxed) {
-        match interruptible_recv(&sock, &mut buf, &mut interrupt_receiver, global_int, should_stop.clone()).await {
+        match interruptible_recv(
+            &sock,
+            &mut buf,
+            &mut interrupt_receiver,
+            global_int,
+            should_stop.clone(),
+        )
+        .await
+        {
             Ok(Ok((size, addr))) => {
                 debug!("Client UDP recv from {:?}, size: {}", &addr, size);
 
                 if let Some(last) = last_addr {
                     if last != addr {
-                        return Err(Error::new(std::io::Error::new(ErrorKind::Other, "Address changed unexpectedly")));
+                        error!("Client UDP recv error addr {} vs {}", last, addr);
+                        return Err(Error::new(io::Error::new(
+                            ErrorKind::Other,
+                            "Address changed unexpectedly",
+                        )));
                     }
                 } else {
                     last_addr = Some(addr);
                     if let Some(sender) = interrupt_sender.take() {
-                        spawn_reader(out_stream.clone(), sock.clone(), addr, should_stop.clone(), sender);
+                        info!("Client UDP from {:?}", &addr);
+                        spawn_reader(
+                            out_stream.clone(),
+                            sock.clone(),
+                            addr,
+                            should_stop.clone(),
+                            sender,
+                        );
                     }
                     // spawn_reader(out_stream.clone(), sock.clone(), addr, should_stop.clone(), interrupt_sender);
                 }
 
-                if let Err(e) = tx.send(UdpReq {
-                    auth: auth.clone(),
-                    payload: buf[..size].to_vec(),
-                }).await {
+                if let Err(e) = tx
+                    .send(UdpReq {
+                        auth: auth.clone(),
+                        payload: buf[..size].to_vec(),
+                    })
+                    .await
+                {
                     error!("Client grpc write error: {:?}", e);
                     return Err(e.into());
                 }
@@ -127,30 +150,17 @@ pub async fn start(l_addr: String, d_addr: String, auth: String, global_int: &mu
             }
             Err(_) => continue,
         }
-        // let (size, addr) = sock.recv_from(&mut buf).await?;
-        // debug!("Client UDP recv from {:?}, size: {}", &addr, size);
-        //
-        // if let Some(last) = last_addr {
-        //     if last != addr {
-        //         return Err(Error::new(std::io::Error::new(ErrorKind::Other, "Address changed unexpectedly")));
-        //     }
-        // } else {
-        //     last_addr = Some(addr);
-        //     spawn_reader(out_stream.clone(), sock.clone(), addr, should_stop.clone());
-        // }
-        //
-        // if let Err(e) = tx.send(UdpReq {
-        //     auth: auth.clone(),
-        //     payload: buf[..size].to_vec(),
-        // }).await {
-        //     error!("Client TCP write error: {:?}", e);
-        //     return Err(e.into());
-        // }
     }
     Ok(())
 }
 
-fn spawn_reader(out_stream: Arc<Mutex<tonic::Streaming<UdpRes>>>, sock: Arc<UdpSocket>, addr: SocketAddr, should_stop: Arc<AtomicBool>, interrupt: Sender<()>) {
+fn spawn_reader(
+    out_stream: Arc<Mutex<tonic::Streaming<UdpRes>>>,
+    sock: Arc<UdpSocket>,
+    addr: SocketAddr,
+    should_stop: Arc<AtomicBool>,
+    interrupt: Sender<()>,
+) {
     spawn(async move {
         while let Some(result) = out_stream.lock().await.next().await {
             match result {
@@ -224,7 +234,13 @@ async fn client_test() -> Result<(), Box<dyn std::error::Error>> {
     // let read_buf = String::from_utf8(read_buf.to_vec()).unwrap();
     // println!("client udp recv {:?}", read_buf);
     let (interrupter, mut interrupt_receiver) = oneshot::channel();
-    let x = start("127.0.0.1:50051".to_string(), "https://127.0.0.1:443".to_string(), "test".to_string(), &mut interrupt_receiver).await;
+    let x = start(
+        "127.0.0.1:50051".to_string(),
+        "https://127.0.0.1:443".to_string(),
+        "test".to_string(),
+        &mut interrupt_receiver,
+    )
+    .await;
     match x {
         Err(e) => {
             error!("{:?}", e);
